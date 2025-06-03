314e481ba3cc39e6a1f28425c953345935836983
Matteo <matteo.vare@gmail.com>
2025-06-03 12:12:27 +0200

    max_d

--- functions/compressione.py
+++ functions/compressione.py
@@ -10,8 +10,11 @@ def imageCompression(filepath):
     img = Image.open(filepath).convert("L")
     img_array = np.array(img)
 
-    F=simpledialog.askinteger("Blocco DCT", "Inserisci F (min: 1 , max:128)", minvalue=1,maxvalue=128)
+    F=simpledialog.askinteger("Blocco DCT", "Inserisci F (min: 1 , max:128):", minvalue=1,maxvalue=128)
     if F is None:
         return
     
-    
+    max_d =2*F-2
+    d = simpledialog.askinteger("Soglia Frequenze", f"inserisci d (min: 0 max: {max_d}):", minvalue=0, maxvalue=max_d)
+    if d is None:
+        return
