f5049dddd5be4e9778f2814163a2e32b49fe3150
Matteo <matteo.vare@gmail.com>
2025-06-09 18:12:09 +0200

    comments + testing

--- functions/compressione.py
+++ functions/compressione.py
@@ -7,8 +7,8 @@ from dd2 import dct_2D, idct_2D
 
 
 def imageCompression(filepath):
-    img = Image.open(filepath).convert("L")
-    img_array = np.array(img)
+    img = Image.open(filepath).convert("L") #converte l'immagine in bianco  nero
+    img_array = np.array(img) # è una matrice np con valori da 0 a 255, prende come input l'imm BW
 
     F=simpledialog.askinteger("Blocco DCT", "Inserisci F (min: 1 , max:128):", minvalue=1,maxvalue=128)
     if F is None:
@@ -19,26 +19,27 @@ def imageCompression(filepath):
     if d is None:
         return
     #ritaglio immagine in multiplidi F
-    h, w =img_array.shape
-    H = h- (h%F)
+    h, w =img_array.shape #altezza,larghezza immagine convertita
+    #arrotondano pr difetto altezza e larghezza al multiplo piu vicino ad F = valore - modulo(valore,F)
+    H = h- (h%F) 
     W = w- (w%F)
-    img_crop= img_array[:H,:W]
-    #prep imm risultante
-    result = np.zeros_like(img_crop, dtype=np.float32)
+    img_crop= img_array[:H,:W] # crea nuova immagine senza i pixel extra nei bordi
+    #inizializzazione imm risultante con una matrice vuota
+    result = np.zeros_like(img_crop, dtype=np.float32) 
 
     #applicazione dct2 ed idct2 per blocchi
     for i in range(0,H,F):
         for j in range(0,W,F):
-            block=img_crop[i:i+F,j:j+F]
+            block=img_crop[i:i+F,j:j+F]  # divisione in blocchi FxF
             c=dct_2D(block)
             #eliminazione frequenze con k+l >= d
             for k in range(F):
                 for l in range(F):
                     if k+l >= d:
-                        c[k,l]=0
-            f_recon = idct_2D(c)
-            f_recon = np.rint(f_recon).clip(0, 255)
-            result[i:i+F, j:j+F] = f_recon
+                        c[k,l]=0 #eliminazione di frequenze k+l >=d
+            f_recon = idct_2D(c) #ricostruzione blocco vi idct
+            f_recon = np.rint(f_recon).clip(0, 255) #blocco ricostruito ed arrotondato e normalizzato 0,255
+            result[i:i+F, j:j+F] = f_recon # inserimento nell' immagine finale
     
     #mostr risultato
     fig, axs = plt.subplots(1,2,figsize=(10,5)) 
